a2da212a392269cd7e1fb48ade7564fb0d764705
farbenweg.ch <54233936+farbenweg@users.noreply.github.com>
2023-06-29 21:51:05 +0200

    Delete Quadrat.js

--- atelier/Quadrat.js
+++
@@ -1,9 +0,0 @@
-function Quadrat() {
-    let Eingabe  = document.getElementById('Eingabe');
-    lt Ergebnis = Eingabe.value * Eingabe.value;
-    alert('Das Quadrat von ' + Eingabe.value + ' = ' + Ergebnis);
-    Eingabe.value = 0;
-   }
-  
-  let los  = document.getElementById('los');
-  los.addEventListener ('click', Quadrat, true);
